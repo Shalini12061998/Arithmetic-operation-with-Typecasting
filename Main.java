e9c368f656fbf3275e4a91698752d36d811d13d6
Shalini12061998 <159100660+Shalini12061998@users.noreply.github.com>
2025-05-22 14:02:44 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,18 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         int marks = 75;
-
-
-        if (marks >= 90) {
-            System.out.println("A grade");
-        } else if (marks >= 75) {
-            System.out.println("B grade");
-        } else if (marks >= 65) {
-            System.out.println("C grade");
-        } else if (marks >= 45) {
-            System.out.println("D grade");
-        } else {
-            System.out.println("Grade: F");
+          double d = 75.5d;
+          double d1= marks+d;
+        {
+            System.out.println(d1);
         }
     }
 }
